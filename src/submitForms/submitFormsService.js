5ee318aa53067c2c346c62a0fa5b72d9bb135c71
moonyaeyoon <moonyaeyoon@naver.com>
2024-03-01 01:48:44 +0900

    chore: 불필요한 코드 제거

--- src/submitForms/submitFormsService.js
+++ src/submitForms/submitFormsService.js
@@ -7,59 +7,7 @@ const userProvider = require('../users/userProvider');
 const submitFormsProvider = require('../submitForms/submitFormsProvider');
 
 /*내가 제출한 지원서 불러오기*/
-exports.getMySubmitForm = async (userId, submitId) => {
-    try {
-        const user = await userProvider.findExistUser(userId);
-        if (!user) {
-            throw new Error(baseResponse.MEMBER_NOT_FOUND);
-        }
-        const submitForm = await submitFormsProvider.getMySubmitForm(user.id, submitId);
-        const form = await submitForm.getForm();
-        const responses = await submitForm.getResponses();
-        await Promise.all(
-            responses.map(async (response) => {
-                const question = await response.getQuestion();
-                const formData = {
-                    question: {
-                        id: question.id,
-                        content: question.content,
-                        type: question.type,
-                    },
-                    response: {
-                        id: response.id,
-                    },
-                };
-                // 객관식인 경우 선택지 정보 추가
-                if (question.type === 'SINGLE' || question.type === 'MULTIPLE') {
-                    const selections = await question.getSelections();
-                    formData.question.selections = selections.map((selection) => ({
-                        id: selection.id,
-                        content: selection.content,
-                    }));
-
-                    //응답에 선택한 선택지 정보 추가
-                    const selectedOptionIds = response.content.split(',');
-                    //selections 배열을 순회하면서 각 선택지의 ID가 selectedOptionIds 배열에 포함되어 있는지 확인
-                    const selectedOptions = selections.filter((selection) => selectedOptionIds.includes(String(selection.id)));
-                    formData.response.selections = selectedOptions.map((option) => ({
-                        id: option.id,
-                        content: option.content,
-                    }));
-                } else {
-                    // 주관식인 경우 응답의 내용(content) 추가
-                    formData.response.content = response.content;
-                }
-            })
-        );
-        return response(baseResponse.SUCCESS_GET_FORM, {
-            id: form.id,
-            title: form.title,
-            formData: formData,
-        });
-    } catch (error) {
-        console.error(error);
-    }
-};
+/*개별 지원서 불러오기*/
 exports.getIndividualSubmitForm = async (userId, submitId) => {
     try {
         const user = await userProvider.findExistUser(userId);
@@ -115,40 +63,6 @@ const formData = async (submitForm) => {
     );
     return formData;
 };
-/*
-const formData = {
-            id: form.id,
-            title: form.title,
-            questions: await Promise.all(
-                form.Questions.map(async (question) => {
-                    if (question.type === 'SINGLE' || question.type === 'MULTIPLE') {
-                        // 객관식인 경우 선택지도 함께 반환
-                        return {
-                            id: question.id,
-                            content: question.content,
-                            type: question.type,
-                            isNecessary: question.isNecessary,
-                            selections: await Promise.all(
-                                question.Selections.map(async (selection) => ({
-                                    id: selection.id,
-                                    content: selection.content,
-                                }))
-                            ),
-                        };
-                    } else {
-                        // 객관식이 아닌 경우 선택지는 반환하지 않음
-                        return {
-                            id: question.id,
-                            content: question.content,
-                            type: question.type,
-                            isNecessary: question.isNecessary,
-                        };
-                    }
-                })
-            ),
-        };
-        return response(baseResponse.SUCCESS_GET_FORM, formData);
-*/
 
 //제출한 지원서 전체 불러오기 (운영진)
 exports.getAllSubmitForms = async (user, formId) => {
